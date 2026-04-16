34d4b1b28d958127bccb6e1d5bf82dd1e7758860
ky0shir089 <ky0shir0_13@yahoo.co.id>
2026-04-16 12:06:41 +0700

    feat: add Byad and Settlement routes, update syncStatus method in SppV2Controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\AuctionCustomerController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BankAccountController;
 use App\Http\Controllers\BankController;
+use App\Http\Controllers\ByadAttachmentController;
+use App\Http\Controllers\ByadController;
+use App\Http\Controllers\ByadPaymentController;
 use App\Http\Controllers\ChartOfAccountController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\GeneralLedgerController;
@@ -22,6 +25,7 @@ use App\Http\Controllers\RvClassificationController;
 use App\Http\Controllers\RvController;
 use App\Http\Controllers\RvUploadController;
 use App\Http\Controllers\SelectController;
+use App\Http\Controllers\SettlementController;
 use App\Http\Controllers\SppController;
 use App\Http\Controllers\SppV2Controller;
 use App\Http\Controllers\SupplierController;
@@ -30,6 +34,7 @@ use App\Http\Controllers\TypeTrxController;
 use App\Http\Controllers\UploadRvController;
 use App\Http\Controllers\UploadSppController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WorkflowHeaderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -92,6 +97,7 @@ Route::middleware('auth:sanctum')
                         Route::resource('rv-classification', RvClassificationController::class);
                         Route::get('invoice-inbox', [InvoiceController::class, 'inbox']);
                         Route::get('memo-invoice/{invoice}', MemoInvoiceController::class);
+                        Route::resource('settlement', SettlementController::class);
                     });
 
                 Route::prefix('v2')
@@ -111,13 +117,16 @@ Route::middleware('auth:sanctum')
                         Route::post('upload-spp', UploadSppController::class);
                         Route::get('memo-payment/{payment}', MemoPaymentController::class);
                         Route::resource('spp', SppController::class);
+                        Route::resource('byad', ByadController::class);
+                        Route::resource('byad-payment', ByadPaymentController::class);
+                        Route::get('byad-attachment/{byad}', ByadAttachmentController::class);
                     });
 
                 Route::prefix('v2')
                     ->group(function () {
                         Route::get('spp-v2', [SppV2Controller::class, 'index']);
                         Route::get('spp-v2/{sppV2}', [SppV2Controller::class, 'show']);
-                        Route::post('spp-v2/sync-status', [SppV2Controller::class, 'syncStatus']);
+                        Route::post('spp-v2/sync-status', [SppV2Controller::class, 'update']);
                     });
             });
 
@@ -139,6 +148,11 @@ Route::middleware('auth:sanctum')
                         Route::get('supplier', [SelectController::class, 'supplier']);
                         Route::get('pph', [SelectController::class, 'pph']);
                         Route::get('rv', [SelectController::class, 'rv']);
+                        Route::get('byad-unit', [SelectController::class, 'byadUnit']);
+                        Route::get('branch', [SelectController::class, 'branch']);
+                        Route::get('byad', [SelectController::class, 'byad']);
+                        Route::get('user', [SelectController::class, 'user']);
+                        Route::get('prepayment', [SelectController::class, 'prepayment']);
                     });
             });
 
@@ -151,6 +165,14 @@ Route::middleware('auth:sanctum')
                         Route::post('report-bank', [ReportController::class, 'reportBank']);
                         Route::post('report-gl', [ReportController::class, 'reportGl']);
                         Route::post('report-kas', [ReportController::class, 'reportKas']);
+                    });
+            });
+
+        Route::prefix('workflow')
+            ->group(function () {
+                Route::prefix('v1')
+                    ->group(function () {
+                        Route::resource('workflow', WorkflowHeaderController::class);
                     });
             });
     });
